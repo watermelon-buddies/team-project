785073f8622e8c5cd3e59443d6a92ccdff94b6bb
sofiaaj <sofia.m.jamesson@gmail.com>
2019-08-09 13:36:16 -0700

    Testing

--- app/src/main/java/com/example/buckit/activities/HomeActivity.java
+++ app/src/main/java/com/example/buckit/activities/HomeActivity.java
@@ -300,7 +300,7 @@ public class HomeActivity extends AppCompatActivity
                                 break;
                             case R.id.action_events:
                                 Bundle bundle = new Bundle();
-/*                                SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+/*                              
                                 Float latitude = sharedPreferences.getFloat("latitude", (float) 37.483149);
                                 Float longitude = sharedPreferences.getFloat("longitude", (float) -122.150028);*/
                                 if (mCurrentLocation != null){
